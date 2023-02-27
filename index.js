8e984882c562fff0bff150345d63642ed09b472f
honmx <max.klim2004@mail.ru>
2023-02-27 23:59:12 +0500

    refactor

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ menu.addEventListener("click", (e) => {
   document.body.classList.toggle("lock");
 })
 
+// ========
+
 const footerTitles = document.querySelectorAll(".footer__title");
 
 footerTitles.forEach(title => {
@@ -13,3 +15,38 @@ footerTitles.forEach(title => {
     e.target.closest(".footer__title").nextElementSibling.classList.toggle("active");
   })
 });
+
+// ========
+
+const qualityImg = document.querySelector(".quality__img");
+const slide = document.querySelector(".slider__slide");
+const arrows = document.querySelectorAll(".slider__arrow");
+
+const imagesPaths = [
+  "assets/img/photo_banner_1.jpg",
+  "assets/img/photo_banner_2.jpg",
+  "assets/img/photo_banner_3.jpg",
+  "assets/img/photo_banner_4.jpg",
+  "assets/img/photo_banner_5.jpg",
+];
+
+let currentImgIndex = 0;
+
+arrows.forEach(arrow => {
+  arrow.addEventListener("click", (e) => {
+    if (e.target.classList.contains("slider__left-arrow") &&
+        currentImgIndex !== 0) {
+      qualityImg.src = imagesPaths[--currentImgIndex];
+    }
+    if (e.target.classList.contains("slider__right-arrow") &&
+        currentImgIndex !== imagesPaths.length - 1) {
+      qualityImg.src = imagesPaths[++currentImgIndex];
+    }
+
+    slide.textContent = ("0" + (currentImgIndex + 1)).slice(-2);
+  })
+})
+
+qualityImg.src = imagesPaths[currentImgIndex];
+
+// ========
